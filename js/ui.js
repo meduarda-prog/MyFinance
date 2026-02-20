2bf55a80b3523b20b844f34dac1e002564b1ff10
EduardaDuDu <17m.eduarda@gmail.com>
2026-02-20 16:34:55 -0300

    ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,21 @@
+const transactionList = document.getElementById("transaction-list");
+const balanceElement = document.getElementById("balance");
 
+function renderTransactions(transactions) {
+  transactionList.innerHTML = "";
+
+  transactions.forEach((transaction, index) => {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+      ${transaction.description} - R$ ${transaction.amount.toFixed(2)}
+      <button onclick="removeTransaction(${index})">X</button>
+    `;
+
+    transactionList.appendChild(li);
+  });
+}
+
+function updateBalance(balance) {
+  balanceElement.textContent = balance.toFixed(2);
+}
