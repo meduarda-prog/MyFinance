d93864b59c58d2448e976b97deee4a3ad203cfb3
EduardaDuDu <17m.eduarda@gmail.com>
2026-02-20 16:30:10 -0300

    calculations.js

--- js/calculations.js
+++ js/calculations.js
@@ -1,1 +1,17 @@
+function calculateMonthlyTotals(transactions) {
+  let income = 0;
+  let expense = 0;
 
+  transactions.forEach(t => {
+    if (t.type === "renda") {
+      income += t.value;
+    } else {
+      expense += t.value;
+    }
+  });
+
+  const balance = income - expense;
+  const percentage = income > 0 ? ((expense / income) * 100).toFixed(2) : 0;
+
+  return { income, expense, balance, percentage };
+}
