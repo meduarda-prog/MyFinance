78aca7f2b0a5c6ec8ee14b152d5eda60b5af07e5
EduardaDuDu <17m.eduarda@gmail.com>
2026-02-20 16:30:48 -0300

    app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  initApp();
+});
 
+function initApp() {
+  const { year, month } = getCurrentYearMonth();
+  console.log("App iniciado para:", year, month);
+}
