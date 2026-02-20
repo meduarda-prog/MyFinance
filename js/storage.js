c87d32df08503be2f71f98f64c8d12d2a8e3f911
EduardaDuDu <17m.eduarda@gmail.com>
2026-02-20 16:29:33 -0300

    storage.js

--- js/storage.js
+++ js/storage.js
@@ -4,3 +4,20 @@
     "02": []
   }
 }
+const STORAGE_KEY = "myfinance_data";
+
+function getData() {
+  return JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+}
+
+function saveData(data) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
+
+function getCurrentYearMonth() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+
+  return { year, month };
+}
