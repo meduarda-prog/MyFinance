5a3ca4a1d5347050dc92150504dce5d11916b934
EduardaDuDu <17m.eduarda@gmail.com>
2026-02-25 09:38:52 -0300

    app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,79 @@
+let chart;
+
+document.addEventListener("DOMContentLoaded", () => {
+  initApp();
+});
+
+function initApp() {
+  document
+    .getElementById("btnAdicionar")
+    .addEventListener("click", addTransaction);
+
+  updateDashboard();
+}
+
+function addTransaction() {
+  const description = document.getElementById("descricao").value;
+  const amount = parseFloat(document.getElementById("valor").value);
+  const type = document.getElementById("tipo").value;
+  const category = document.getElementById("categoria").value || "Outros";
+
+  if (!description || isNaN(amount)) {
+    alert("Preencha corretamente!");
+    return;
+  }
+
+  const transaction = {
+    description,
+    amount,
+    type,
+    category
+  };
+
+  const { year, month } = getCurrentYearMonth();
+  const data = getData();
+
+  if (!data[year]) data[year] = {};
+  if (!data[year][month]) data[year][month] = [];
+
+  data[year][month].push(transaction);
+  saveData(data);
+
+  document.getElementById("descricao").value = "";
+  document.getElementById("valor").value = "";
+
+  updateDashboard();
+}
+
+function updateDashboard() {
+  const { year, month } = getCurrentYearMonth();
+  const data = getData();
+
+  const transactions =
+    data[year] && data[year][month]
+      ? data[year][month]
+      : [];
+
+  const totals = calculateMonthlyTotals(transactions);
+
+  updateCards(totals);
+  updateChart(totals);
+}
+
+function updateCards(totals) {
+  document.getElementById("totalReceita").textContent =
+    `R$ ${totals.income.toFixed(2)}`;
+
+  document.getElementById("totalDespesa").textContent =
+    `R$ ${totals.expense.toFixed(2)}`;
+
+  document.getElementById("saldo").textContent =
+    `R$ ${totals.balance.toFixed(2)}`;
+
+  document.getElementById("percentual").textContent =
+    `${totals.percent}%`;
+}
+
 function updateChart(totals) {
   const ctx = document.getElementById("grafico");
   const lista = document.getElementById("listaCategorias");
@@ -38,7 +114,14 @@ function updateChart(totals) {
     data: {
       labels: labels,
       datasets: [{
-        data: data
+        data: data,
+        backgroundColor: [
+          "#2563eb",
+          "#1d4ed8",
+          "#3b82f6",
+          "#60a5fa",
+          "#93c5fd"
+        ]
       }]
     },
     options: {
